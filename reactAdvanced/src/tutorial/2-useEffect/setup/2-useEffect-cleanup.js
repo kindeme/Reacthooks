6a7f55730cb596731ecefa0f7ef30cfb388de385
Kindeme <fkounoudji@gmail.com>
2020-12-02 23:00:18 -0600

    useEffect

--- reactAdvanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ reactAdvanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,11 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  const [size, setSize] = useState(window.innerWidth);
-  return <h2>useEffect cleanup</h2>;
+	const [size, setSize] = useState(window.innerWidth);
+
+	const checkSize = () => {
+		setSize(window.innerWidth);
+	};
+	useEffect(() => {
+		console.log("hello World");
+    window.addEventListener("resize", checkSize);
+    return() =>{
+      console,log('cleanup');
+      winndow.removeEventListener('resize',checkSize);
+    }
+  });
+  console.log('render');
+	return (
+		<>
+			<h1>window</h1>
+			<h2>{size}</h2>
+		</>
+	);
 };
 
 export default UseEffectCleanup;
