9764d93244fbf665f181f7128e945fa878fab599
Kindeme <fkounoudji@gmail.com>
2020-12-03 23:59:52 -0600

    fetch data

--- reactAdvanced/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ reactAdvanced/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,25 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
-
+const url = "https://api.github.com/users";
 
 //second argument
 
 const UseEffectFetchData = () => {
-  const[users, setUsers] = setState([]);
-  const getUsers = async () =>{
-    const response = await fatch(url);
+	const [users, setUsers] = setState([]);
+	const getUsers = async () => {
+		const response = await fatch(url);
     const users = await response.json();
-    console.log(users);
-  };
-  useEffect(() =>{
-getUsers();
-  });
-  return (
-    <>
-  <h2>github users</h2>
-    </>
-  );
+    setUsers(users)
+		//console.log(users);
+	};
+	useEffect(() => {
+		getUsers();
+	},[]);
+	return (
+		<>
+			<h2>github users</h2>
+			<ul>{users.map((user) =>{
+        const {id,login,avatar_url,html_url} = user;
+        return(
+          <li key ={id}>
+            <img src={avatar_url} alt={login}/>
+            <div>
+              <h4>{login}</h4>
+              <a href={html_url}></a>
+            </div>
+          </li>
+
+        )
+      })}</ul>
+		</>
+	);
 };
 
 export default UseEffectFetchData;
