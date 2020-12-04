135ad67304990a8a0ed2058013dbfc784fa54d41
Kindeme <fkounoudji@gmail.com>
2020-12-04 00:26:21 -0600

    multiple render

--- reactAdvanced/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ reactAdvanced/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-	const [loading, setLoading] = useState(false);
-	if (loading) {
-		return <h2> Loading ...</h2>;
-	}
+  const [isLoading, setIsLoading] = useState(true);
+  const[isError,setIsError] = useState(false);
+const [user,setUser] = useState('default user')
+
+useEffect(() =>{
+  fetch(url)
+  .then((resp) => resp.json())
+  .then((user) => console.log(user))
+  .catch((error) => console.log(error));
+},[]);
+
+	if (isLoading) {
+		return (
+			<div>
+				<h1> Loading ...</h1>
+			</div>
+		);
+  }
+  	if (isError) {
+			return (
+				<div>
+					<h1> Error ...</h1>
+				</div>
+			);
+		}
 	return <h2>multiple returns</h2>;
 };
 
