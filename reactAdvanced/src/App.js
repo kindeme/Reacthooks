6884cbf4feb0e97f667059aed86d8a6c07261ecf
kindeme <fkounoudji@gmail.com>
2020-12-28 01:28:49 -0600

    useState

--- reactAdvanced/src/App.js
+++ reactAdvanced/src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
+import React from "react";
+//import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
 //import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
 //import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
 //import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
 //import Final from "./tutorial/1-useState/final/3-useState-array";
 //import Setup from "./tutorial/1-useState/setup/5-useState-counter";
 //import Setup from "./tutorial/1-useState/setup/4-useState-object";
-// import Setup from "./tutorial/1-useState/setup/3-useState-array";
-//import Setup from './tutorial/1-useState/setup/2-useState-basics';
-
-
+import Setup from "./tutorial/1-useState/setup/3-useState-array";
+//import Setup from "./tutorial/1-useState/setup/2-useState-basics";
+//import Setup from "./tutorial/1-useState/setup/1-error-example";
 
 function App() {
   return (
-		<div className='container'>
-			<Setup />
-		</div>
-	);
+    <div className="container">
+      <Setup />
+    </div>
+  );
 }
 
-export default App
+export default App;
