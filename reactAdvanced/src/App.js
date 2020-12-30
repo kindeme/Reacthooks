0ef592086ab34102a6f35a65d1d62f146e3b8c06
kindeme <fkounoudji@gmail.com>
2020-12-30 06:09:02 -0600

    show/hide component

--- reactAdvanced/src/App.js
+++ reactAdvanced/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
+//import Setup from "./tutorial / 3 - conditional - rendering / setup
+//import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
 //import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 //import Setup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
 //import Setup from "./tutorial/2-useEffect/setup/1-useEffect-basics";
