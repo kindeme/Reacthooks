69c716134bca1ca53e4df99f443c202b79658bc9
kindeme <fkounoudji@gmail.com>
2021-01-14 13:48:51 -0600

    usereducer

--- reactAdvanced/src/tutorial/6-useReducer/setup/index.js
+++ reactAdvanced/src/tutorial/6-useReducer/setup/index.js
@@ -3,7 +3,25 @@ import Modal from "./Modal";
 import { data } from "../../../data";
 // reducer function
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  const newPeople = [...state.people, action.playload];
+  if (action.type === "ADD_ITEM") {
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "item added",
+    };
+  }
+  if (action.type === "NO_VALUE") {
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: "Please enter value",
+    };
+  }
+  throw new Error("no matching action type");
+};
 
 const defaultState = {
   people: [],
@@ -17,7 +35,10 @@ const Index = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
+      dispatch({ type: "ADD_ITEM", payload: newPeople });
+      setName("");
     } else {
+      dispatch({ type: "NO_VALUES" });
     }
   };
   return (
